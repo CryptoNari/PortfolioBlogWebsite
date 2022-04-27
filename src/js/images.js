61f41ebfdeb2c3a7a29b9be7aac4d479e1603eeb
Andreas Vieten <cryptonari@outlook.com>
2022-04-27 22:16:19 +0200

    blog section draft

--- src/js/images.js
+++ src/js/images.js
@@ -1,5 +1,8 @@
 import projectImage1 from '../img/port1.jpg';
 
 var projectImg1 = document.getElementById('project-img-1');
+var blogImg1 = document.getElementById('blog-img-1');
+
 
 projectImg1.src = projectImage1;
+blogImg1.src = projectImage1;
