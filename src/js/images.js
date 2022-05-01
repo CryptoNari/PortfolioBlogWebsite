3d759e4c8550b6d4db58c498965c0c9366495196
Andreas Vieten <cryptonari@outlook.com>
2022-05-01 21:12:38 +0200

    section home draft

--- src/js/images.js
+++ src/js/images.js
@@ -1,8 +1,10 @@
+import homeImage from '../img/home.jpg';
 import projectImage1 from '../img/port1.jpg';
 
+var homeImg1 = document.getElementById('img-home');
 var projectImg1 = document.getElementById('project-img-1');
 var blogImg1 = document.getElementById('blog-img-1');
 
-
+homeImg1.src = homeImage;
 projectImg1.src = projectImage1;
 blogImg1.src = projectImage1;
