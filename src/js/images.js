183041d61aad290c85aa911773079d94d55335cc
Andreas Vieten <cryptonari@outlook.com>
2022-05-04 22:02:16 +0200

    add portfolio section items

--- src/js/images.js
+++ src/js/images.js
@@ -1,10 +1,19 @@
 import homeImage from '../img/home.jpg';
 import projectImage1 from '../img/port1.jpg';
+import projectImage2 from '../img/port2.jpg';
+import projectImage3 from '../img/port3.jpg';
+import projectImage4 from '../img/port4.jpg';
 
 var homeImg1 = document.getElementById('img-home');
 var projectImg1 = document.getElementById('project-img-1');
+var projectImg2 = document.getElementById('project-img-2');
+var projectImg3 = document.getElementById('project-img-3');
+var projectImg4 = document.getElementById('project-img-4');
 var blogImg1 = document.getElementById('blog-img-1');
 
 homeImg1.src = homeImage;
-projectImg1.src = projectImage1;
+projectImg1.src = homeImage;
+projectImg2.src = projectImage2;
+projectImg3.src = projectImage3;
+projectImg4.src = projectImage4;
 blogImg1.src = projectImage1;
