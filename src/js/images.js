ff7e23e3e1ac992a82e922768370fb6c56c12c18
Andreas Vieten <cryptonari@outlook.com>
2022-05-05 15:51:43 +0200

    add card to blog section

--- src/js/images.js
+++ src/js/images.js
@@ -3,6 +3,8 @@ import projectImage1 from '../img/port1.jpg';
 import projectImage2 from '../img/port2.jpg';
 import projectImage3 from '../img/port3.jpg';
 import projectImage4 from '../img/port4.jpg';
+import blogImage1 from '../img/blog3.jpg';
+import blogImage2 from '../img/blog2.jpg';
 
 var homeImg1 = document.getElementById('img-home');
 var projectImg1 = document.getElementById('project-img-1');
@@ -10,10 +12,12 @@ var projectImg2 = document.getElementById('project-img-2');
 var projectImg3 = document.getElementById('project-img-3');
 var projectImg4 = document.getElementById('project-img-4');
 var blogImg1 = document.getElementById('blog-img-1');
+//var blogImg2 = document.getElementById('blog-img-2');
 
 homeImg1.src = homeImage;
 projectImg1.src = homeImage;
 projectImg2.src = projectImage2;
 projectImg3.src = projectImage3;
 projectImg4.src = projectImage4;
-blogImg1.src = projectImage1;
+blogImg1.src = blogImage1;
+//blogImg2.src = blogImage2;
