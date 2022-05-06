b376c6cd6cbb0bebfb2ad61cd07b7afbecc1b978
Andreas Vieten <cryptonari@outlook.com>
2022-05-06 13:22:12 +0200

    deployment heroku fixed

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use(express.static('dist'));
 
 // send the user to index html page inspite of the url
-app.get('*', (req, res) => {
+app.get('*', (req, res) => { 
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
-app.listen(port);
+app.listen(port, 
+	() => console.log("Server is running..."));
