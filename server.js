0f2da1f1abd20a78f5da41ddfbdf6b5c0e5e93a2
Andreas Vieten <cryptonari@outlook.com>
2022-05-12 20:28:02 +0200

    add nodemailer functions contact form

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
+require('dotenv').config();
+
 const express = require('express');
+const nodemailer = require('nodemailer');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// the __dirname is the current directory from where the script is running
+// Middleware
 app.use(express.static('dist'));
+app.use(express.json());
 
 // send the user to index html page inspite of the url
-app.get('*', (req, res) => { 
+app.get('/', (req, res) => { 
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
-app.listen(port, 
-	() => console.log("Server is running..."));
+app.post('/', (req, res) => { 
+  const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+      user: process.env.NODEMAILER_SENDER,
+      pass: process.env.NODEMAILER_SENDER_PASS
+    }
+  })
+
+  const mailOptions = {
+    from: process.env.NODEMAILER_SENDER,
+    to: process.env.NODEMAILER_RECEIVER,
+    subject: `Message from ${req.body.email}:  ${req.body.subject}`,
+    text: req.body.message
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error){
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('success');
+    }
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/js/controls.js
+++ src/js/controls.js
@@ -1,5 +1,12 @@
 const sections = document.querySelectorAll('section');
 const ctrlBtns = document.querySelectorAll('.control-btn');
+const contactForm = document.querySelector('.contact-form');
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
 
 const pageControls = () => {
     //Button Click active class/section
@@ -22,6 +29,35 @@ const pageControls = () => {
         element.classList.toggle('light-mode')
     })
 
+    // ContactForm submit btn
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    
+        let formData = {
+            name: name.value,
+            email: email.value,
+            subject: subject.value,
+            message: message.value
+        }
+        
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', '/');
+        xhr.setRequestHeader('content-type', 'application/json');
+        xhr.onload = function () {
+            console.log(xhr.responseText);
+            if(xhr.responseText == 'success') {
+                alert('Email sent');
+                name.value = '';
+                email.value = '';
+                subject.value = '';
+                message.value = '';
+            } else {
+                alert('Email could not be sent!')
+            }
+        }
+
+        xhr.send(JSON.stringify(formData));
+    })
 }
 
 export {pageControls};
